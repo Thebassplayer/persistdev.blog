9a6635a3efc093649865fc895d73d0b8475a2911
Roy Lopez <roylopezdev@gmail.com>
2024-01-20 18:55:02 -0300

    Table of content

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,6 +4,12 @@ import RenderMdx from "@/src/components/Blog/RenderMdx";
 import Tag from "@/src/components/Elements/Tag";
 import Image from "next/image";
 
+type Heading = {
+  level: "one" | "two" | "three" | "four" | "five" | "six";
+  slug: string;
+  text: string;
+};
+
 const BlogPage = ({ params }: { params: { slug: string } }) => {
   const blog = allBlogs.find(blog => blog._raw.flattenedPath === params.slug);
 
@@ -39,7 +45,42 @@ const BlogPage = ({ params }: { params: { slug: string } }) => {
       </div>
       {blog ? <BlogDetails blog={blog} slug={params.slug} /> : null}
       <div className="grid grid-cols-12 gap-16 mt-8 px-10">
-        <div className="col-span-4"></div>
+        <div className="col-span-4">
+          <details
+            className="border border-solid border-dark text-dark rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
+            open
+          >
+            <summary className="text-lg ont-semibold capitalize cursor-pointer">
+              Table of Content
+            </summary>
+            <ul className="mt-4 font-in text-base">
+              {blog?.toc.map((heading: Heading) => {
+                return (
+                  <li key={`#${heading.slug}`} className="py-1">
+                    <a
+                      href={`#${heading.slug}`}
+                      data-level={heading.level}
+                      className="data-[level=two]:pl-0  data-[level=two]:pt-2
+                                       data-[level=two]:border-t border-solid border-dark/40
+                                       data-[level=three]:pl-4
+                                       sm:data-[level=three]:pl-6
+                                       flex items-center justify-start
+                                       "
+                    >
+                      {heading.level === "three" ? (
+                        <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                          &nbsp;
+                        </span>
+                      ) : null}
+
+                      <span className="hover:underline">{heading.text}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </details>
+        </div>
         {blog ? <RenderMdx blog={blog} /> : null}
       </div>
     </article>
