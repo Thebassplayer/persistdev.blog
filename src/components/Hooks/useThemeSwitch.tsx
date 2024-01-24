f1e2a2bbbfebe2b6f05b2d5bba07689e028d4974
Roy Lopez <roylopezdev@gmail.com>
2024-01-24 12:29:22 -0300

    Refactor code for improved performance and readability

---
+++ src/components/Hooks/useThemeSwitch.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+const useThemeSwitch = (): [
+  mode: Theme,
+  setMode: React.Dispatch<React.SetStateAction<Theme>>
+] => {
+  const preferDarQuery = "(prefers-color-scheme: dark)";
+  const storageKey = "theme";
+
+  const toggleTheme = (theme: Theme) => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    window.localStorage.setItem(storageKey, theme);
+  };
+
+  const getUserPreference = () => {
+    const userPref = window.localStorage.getItem(storageKey);
+    if (userPref) {
+      return userPref as Theme;
+    }
+    return window.matchMedia(preferDarQuery).matches ? "dark" : "light";
+  };
+
+  const [mode, setMode] = useState<Theme>("dark");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarQuery);
+
+    const handleChange = () => {
+      const newMode = getUserPreference();
+      setMode(newMode);
+      toggleTheme(newMode);
+    };
+    handleChange();
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  return [mode, setMode];
+};
+
+export default useThemeSwitch;
