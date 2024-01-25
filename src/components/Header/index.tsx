427d2b5d472f6f6c753d7853e3f70975f88a7c5d
Roy Lopez <roylopezdev@gmail.com>
2024-01-25 14:17:36 -0300

    Refactor header component and add theme switcher

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,28 @@ import Logo from "./Logo";
 import { socialLinks } from "@/src/constants/SOCIAL_LINKS";
 import Nav from "./Nav";
 import { useState } from "react";
+import Link from "next/link";
+import { MoonIcon, SunIcon } from "../Icons";
+import { cx } from "@/src/utils";
+import useThemeSwitch from "@/src/Hooks/useThemeSwitch";
 
 const Header = () => {
+  const [mode, setMode] = useThemeSwitch();
   const [click, setClick] = useState(false);
 
-  const toggleHamburger = () => setClick(!click);
+  const toggle = () => {
+    setClick(!click);
+  };
   return (
-    <header className="w-full p-4 px-5 sm:px-10 flex items-center justify-between">
+    <header className="w-full p-4  px-5 sm:px-10 flex items-center justify-between">
       <Logo />
+
       <button
-        className="inline-block sm:hidden z-50 cursor-pointer"
-        onClick={toggleHamburger}
+        className="inline-block sm:hidden z-50"
+        onClick={toggle}
+        aria-label="Hamburger Menu"
       >
-        <div className="w-6 transition-all ease duration-300">
+        <div className="w-6 cursor-pointer transition-all ease duration-300">
           <div className="relative">
             <span
               className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
@@ -48,7 +57,69 @@ const Header = () => {
           </div>
         </div>
       </button>
-      <Nav />
+
+      <nav
+        className=" w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center flex  sm:hidden
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50
+        transition-all ease duration-300
+        "
+        style={{
+          top: click ? "1rem" : "-5rem",
+        }}
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mx-2">
+          Contact
+        </Link>
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={cx(
+            "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+          )}
+          aria-label="theme-switcher"
+        >
+          {mode === "light" ? (
+            <MoonIcon className={"fill-dark"} />
+          ) : (
+            <SunIcon className={"fill-dark"} />
+          )}
+        </button>
+      </nav>
+
+      <nav
+        className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center hidden sm:flex
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50"
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mx-2">
+          Contact
+        </Link>
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={cx(
+            "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+          )}
+          aria-label="theme-switcher"
+        >
+          {mode === "light" ? (
+            <MoonIcon className={"fill-dark"} />
+          ) : (
+            <SunIcon className={"fill-dark"} />
+          )}
+        </button>
+      </nav>
       <div className="sm:flex items-center hidden">
         {socialLinks.map(({ label, href, svg }, index) => (
           <a
