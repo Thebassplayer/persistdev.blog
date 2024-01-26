e5ce64acb665b0f3da3fe63184f0218b3b312c40
Roy Lopez <roylopezdev@gmail.com>
2024-01-26 16:38:47 -0300

    Refactor useThemeSwitch hook to handle server-side rendering

--- src/Hooks/useThemeSwitch.tsx
+++ src/Hooks/useThemeSwitch.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useEffect, useState } from "react";
 
 type Theme = "light" | "dark";
@@ -11,42 +10,53 @@ const useThemeSwitch = (): [
   const preferDarkQuery = "(prefers-color-scheme: dark)";
   const storageKey = "theme";
 
+  const isClient = typeof window === "object"; // Check if window is defined
+
   const toggleTheme = (theme: Theme) => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
+    if (isClient) {
+      if (theme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+      window.localStorage.setItem(storageKey, theme);
     }
-    window.localStorage.setItem(storageKey, theme);
   };
 
   const getUserPreference = () => {
-    const userPref = window.localStorage.getItem(storageKey);
-    if (userPref) {
-      return userPref as Theme;
+    if (isClient) {
+      const userPref = window.localStorage.getItem(storageKey);
+      if (userPref) {
+        return userPref as Theme;
+      }
+      return window.matchMedia(preferDarkQuery).matches ? "dark" : "light";
     }
-    return window.matchMedia(preferDarkQuery).matches ? "dark" : "light";
+    // Return default theme for server-side rendering
+    return "light";
   };
 
   const [mode, setMode] = useState<Theme>(() => getUserPreference()); // Initialize with user preference
+
   useEffect(() => {
-    const mediaQuery = window.matchMedia(preferDarkQuery);
+    if (isClient) {
+      const mediaQuery = window.matchMedia(preferDarkQuery);
 
-    const handleChange = () => {
-      const newMode = getUserPreference();
-      setMode(newMode);
-      toggleTheme(newMode);
-    };
-    handleChange();
+      const handleChange = () => {
+        const newMode = getUserPreference();
+        setMode(newMode);
+        toggleTheme(newMode);
+      };
+      handleChange();
 
-    mediaQuery.addEventListener("change", handleChange);
+      mediaQuery.addEventListener("change", handleChange);
 
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
+      return () => mediaQuery.removeEventListener("change", handleChange);
+    }
+  }, [isClient]);
 
   useEffect(() => {
     toggleTheme(mode);
-  }, [mode]);
+  }, [mode, isClient]);
 
   return [mode, setMode];
 };
