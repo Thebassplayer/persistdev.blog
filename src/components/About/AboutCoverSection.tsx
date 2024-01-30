0a53022e4eea57ac82002c6556c42913e62143a2
Roy Lopez <roylopezdev@gmail.com>
2024-01-30 19:03:09 -0300

    Refactor AboutCoverSection and Skills components

--- src/components/About/AboutCoverSection.tsx
+++ src/components/About/AboutCoverSection.tsx
@@ -4,16 +4,16 @@ import profileCharacter from "@/public/AI-Roy.png";
 
 const AboutCoverSection = () => {
   return (
-    <section className="flex w-full flex-row items-center justify-center border-b-2 border-solid border-dark text-dark dark:border-light dark:text-light">
-      <div className="flex h-[400px] w-1/2 flex-row items-center border-r-2 border-solid border-dark dark:border-light">
+    <section className="flex w-full flex-col items-center justify-center border-b-2 border-solid border-dark text-dark dark:border-light dark:text-light md:h-[75vh] md:flex-row">
+      <div className="flex h-[400px] w-full flex-row items-center border-r-2 border-solid border-dark dark:border-light md:w-1/2">
         <Image
           src={profileCharacter}
           alt="Roy codign in a laptop computer"
-          className="h-full w-full object-contain object-center"
+          className="h-full w-4/5 object-contain object-center xs:w-3/4 md:w-full"
         />
       </div>
-      <div className="flex w-1/2 flex-col items-start justify-center px-16 text-left">
-        <h2 className="text-6xl font-bold capitalize">
+      <div className="pb-a1 flex w-full flex-col items-start justify-center px-5 text-left xs:p-10 md:w-1/2 lg:px-16">
+        <h2 className="text-center text-4xl font-bold capitalize xs:text-5xl lg:text-left sxl:text-6xl">
           Dream Big, Work Hard, Achieve More!
         </h2>
         <p className="mt-4 text-base font-medium capitalize">
--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -16,15 +16,15 @@ const SkillList = [
 
 const Skills = () => {
   return (
-    <section className="flex w-full flex-col border-b-2 border-solid border-dark p-20 text-dark dark:border-light dark:text-light">
-      <span className="text-4xl font-semibold text-accent dark:text-accentDark">
+    <section className="flex w-full flex-col border-b-2 border-solid border-dark p-5 text-dark dark:border-light dark:text-light xs:p-10 sm:p-12 md:p-16 lg:p-20">
+      <span className="text-lg font-semibold text-accent dark:text-accentDark sm:text-3xl md:text-4xl">
         I'm confortanble in...
       </span>
-      <ul className="mt-8 flex flex-wrap justify-start">
+      <ul className="mt-8 flex flex-wrap justify-center xs:justify-start">
         {SkillList.map((skill, index) => (
           <li
             key={index}
-            className="ease mb-6 mr-6 inline-block cursor-pointer rounded border-2 border-solid border-dark px-12 py-5 text-2xl font-semibold capitalize transition-all duration-200 hover:scale-105 dark:border-light"
+            className="ease mb-3 mr-3 inline-block cursor-pointer rounded border-2 border-solid border-dark px-4 py-2 text-base font-semibold capitalize transition-all duration-200 hover:scale-105 dark:border-light dark:font-normal xs:mb-4 xs:mr-4 xs:px-6 xs:py-3 xs:text-lg sm:px-8 sm:py-4 sm:text-xl md:mb-6 md:mr-6 md:text-2xl lg:px-12 lg:py-5"
           >
             {skill}
           </li>
