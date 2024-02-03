0650beca169d620de862568ed294b927e742fa7e
Roy Lopez <roylopezdev@gmail.com>
2024-02-03 14:57:39 -0300

    Refactor blog page component to generate metadata on jsonLd

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -29,14 +29,14 @@ export async function generateMetadata({
   const modifiedAt = new Date(
     blog.publishedAt || blog.publishedAt,
   ).toISOString();
-  let imageList: string[] = [siteMetadata.socialBanner];
+  let blogMainImageList: string[] = [siteMetadata.socialBanner];
   if (blog.image && typeof blog.image.filePath === "string") {
-    imageList = [
+    blogMainImageList = [
       siteMetadata.siteUrl + blog.image.filePath.replace("../public", ""),
     ];
   }
 
-  const ogImages = imageList.map((image) => {
+  const ogImages = blogMainImageList.map((image) => {
     return {
       url: image.includes("http") ? image : siteMetadata.siteUrl + image,
     };
@@ -70,73 +70,107 @@ export async function generateMetadata({
 
 const BlogPage = ({ params }: { params: { slug: string } }) => {
   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
+  const firstBlogTag = blog?.tags?.[0];
+  const datePublished = new Date(blog?.publishedAt ?? "").toISOString();
+  const dateModified = new Date(
+    ((blog?.updatedAt ?? " ") || blog?.publishedAt) ?? " ",
+  ).toISOString();
+  const author = blog?.author ? [blog?.author] : siteMetadata.author;
 
-  const firstTag = blog?.tags?.[0];
+  let blogMainImageList: string[] = [siteMetadata.socialBanner];
+  if (blog?.image && typeof blog.image.filePath === "string") {
+    blogMainImageList = [
+      siteMetadata.siteUrl + blog.image.filePath.replace("../public", ""),
+    ];
+  }
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    headline: blog?.title,
+    description: blog?.description,
+    image: blogMainImageList,
+    datePublished: datePublished,
+    dateModified: dateModified,
+    author: [
+      {
+        "@type": "Person",
+        name: author,
+        url: "https://www.roylopez.dev",
+      },
+    ],
+  };
 
   return (
-    <article>
-      <div className="relative mb-8 h-[70vh] w-full bg-dark text-center">
-        <div className="absolute left-1/2 top-1/2 z-10 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
-          {firstTag ? (
-            <Tag
-              name={firstTag}
-              link={`/categories/${slug(firstTag)}`}
-              className="px-6 py-2 text-sm"
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <article>
+        <div className="relative mb-8 h-[70vh] w-full bg-dark text-center">
+          <div className="absolute left-1/2 top-1/2 z-10 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
+            {firstBlogTag ? (
+              <Tag
+                name={firstBlogTag}
+                link={`/categories/${slug(firstBlogTag)}`}
+                className="px-6 py-2 text-sm"
+              />
+            ) : null}
+            <h1 className="relative mt-6 inline-block w-5/6 text-2xl font-semibold capitalize leading-normal text-light md:text-3xl lg:text-5xl">
+              {blog?.title}
+            </h1>
+          </div>
+          <div className="absolute bottom-0 left-0 right-0 top-0 h-full bg-dark/60 dark:bg-dark/40" />
+          {blog?.image ? (
+            <Image
+              src={blog.image?.filePath.replace("../public", "")}
+              alt={blog.title}
+              placeholder="blur"
+              blurDataURL={blog.image?.blurhashDataUrl}
+              height={blog.image?.height}
+              width={blog.image?.width}
+              className="aspect-square h-full w-full object-cover object-center"
             />
           ) : null}
-          <h1 className="relative mt-6 inline-block w-5/6 text-2xl font-semibold capitalize leading-normal text-light md:text-3xl lg:text-5xl">
-            {blog?.title}
-          </h1>
         </div>
-        <div className="absolute bottom-0 left-0 right-0 top-0 h-full bg-dark/60 dark:bg-dark/40" />
-        {blog?.image ? (
-          <Image
-            src={blog.image?.filePath.replace("../public", "")}
-            alt={blog.title}
-            placeholder="blur"
-            blurDataURL={blog.image?.blurhashDataUrl}
-            height={blog.image?.height}
-            width={blog.image?.width}
-            className="aspect-square h-full w-full object-cover object-center"
-          />
-        ) : null}
-      </div>
-      {blog ? <BlogDetails blog={blog} slug={params.slug} /> : null}
-      <div className="mt-8 grid grid-cols-12 gap-y-8 px-5 md:px-10 lg:gap-8 sxl:gap-16">
-        <div className="col-span-12 lg:col-span-4">
-          <details
-            className="sticky top-6 overflow-hidden overflow-y-auto rounded-lg border border-solid border-dark p-4 text-dark dark:border-light dark:text-light md:max-h-[80vh]"
-            open
-          >
-            <summary className="ont-semibold cursor-pointer text-lg capitalize">
-              Table of Content
-            </summary>
-            <ul className="mt-4 font-in text-base">
-              {blog?.toc.map((heading: Heading) => {
-                return (
-                  <li key={`#${heading.slug}`} className="py-1">
-                    <a
-                      href={`#${heading.slug}`}
-                      data-level={heading.level}
-                      className="flex  items-center justify-start border-solid border-dark/40 text-sm data-[level=two]:border-t data-[level=three]:pl-4 data-[level=two]:pl-0 data-[level=two]:pt-2 sm:text-base  sm:data-[level=three]:pl-6 md:text-lg"
-                    >
-                      {heading.level === "three" ? (
-                        <span className="mr-2 flex h-1 w-1 rounded-full bg-dark">
-                          &nbsp;
-                        </span>
-                      ) : null}
+        {blog ? <BlogDetails blog={blog} slug={params.slug} /> : null}
+        <div className="mt-8 grid grid-cols-12 gap-y-8 px-5 md:px-10 lg:gap-8 sxl:gap-16">
+          <div className="col-span-12 lg:col-span-4">
+            <details
+              className="sticky top-6 overflow-hidden overflow-y-auto rounded-lg border border-solid border-dark p-4 text-dark dark:border-light dark:text-light md:max-h-[80vh]"
+              open
+            >
+              <summary className="ont-semibold cursor-pointer text-lg capitalize">
+                Table of Content
+              </summary>
+              <ul className="mt-4 font-in text-base">
+                {blog?.toc.map((heading: Heading) => {
+                  return (
+                    <li key={`#${heading.slug}`} className="py-1">
+                      <a
+                        href={`#${heading.slug}`}
+                        data-level={heading.level}
+                        className="flex  items-center justify-start border-solid border-dark/40 text-sm data-[level=two]:border-t data-[level=three]:pl-4 data-[level=two]:pl-0 data-[level=two]:pt-2 sm:text-base  sm:data-[level=three]:pl-6 md:text-lg"
+                      >
+                        {heading.level === "three" ? (
+                          <span className="mr-2 flex h-1 w-1 rounded-full bg-dark">
+                            &nbsp;
+                          </span>
+                        ) : null}
 
-                      <span className="hover:underline">{heading.text}</span>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </details>
+                        <span className="hover:underline">{heading.text}</span>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </details>
+          </div>
+          {blog ? <RenderMdx blog={blog} /> : null}
         </div>
-        {blog ? <RenderMdx blog={blog} /> : null}
-      </div>
-    </article>
+      </article>
+    </>
   );
 };
 
