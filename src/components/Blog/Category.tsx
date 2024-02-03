22464e622b3108cdf8a1300924c8f5862f76ffb6
Roy Lopez <roylopezdev@gmail.com>
2024-02-03 13:10:44 -0300

    Update Category component styles

--- src/components/Blog/Category.tsx
+++ src/components/Blog/Category.tsx
@@ -16,9 +16,11 @@ const Category = ({ link = "#", name, active, ...props }: CategoryProps) => {
     <Link
       href={link}
       className={cx(
-        "ease m-2 inline-block rounded-full border-2 border-solid  border-dark bg-dark px-10 py-2 duration-200 hover:scale-105",
+        "ease m-1 inline-block rounded-full border-2 border-solid border-dark bg-dark px-3 py-1 text-xs transition-transform duration-200 hover:scale-105 sm:m-2 sm:px-4 sm:text-sm md:px-5 md:py-2 lg:px-6 lg:text-base",
         className,
-        active ? "bg.dark text-light" : "bg-light text-dark",
+        active
+          ? "bg-dark text-light dark:border-light dark:bg-light dark:text-dark"
+          : "bg-light text-dark dark:border-light dark:bg-dark dark:text-light",
       )}
     >
       #{name}
