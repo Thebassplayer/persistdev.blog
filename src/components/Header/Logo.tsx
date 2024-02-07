8490cf38b489c810804a279fc1a42908e637b894
Roy Lopez <roylopezdev@gmail.com>
2024-02-07 14:20:44 -0300

    Update Logo component styles

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = ({ showNavBar }: LogoProps): JSX.Element => (
   <Link href={"/"} className="flex items-center text-dark dark:text-light">
     <div
       className={cx(
-        "ease mr-2 overflow-hidden rounded-full border border-solid border-dark transition-all duration-300 dark:border-light md:mr-4 md:w-16",
+        "ease mr-2 overflow-hidden rounded-full border border-solid border-dark shadow-[rgba(0,_0,_0,_0)_0px_0px_8px]  shadow-light  transition-all duration-300 dark:border-light md:mr-4 md:w-16",
         showNavBar ? "w-8" : "w-12",
       )}
     >
@@ -25,7 +25,7 @@ const Logo = ({ showNavBar }: LogoProps): JSX.Element => (
     </div>
     <span
       className={cx(
-        "text-shadow text-base font-bold shadow-light transition-all duration-300 ease-in-out md:text-xl",
+        "text-shadow dark:text-shadow-none text-base font-bold shadow-light transition-transform duration-300 ease-in-out md:text-xl",
         showNavBar ? "text-transparent" : "",
       )}
     >
