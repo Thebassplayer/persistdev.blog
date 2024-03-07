6da0e19d40a400eced401e0cf53c7da923c205fe
Roy Lopez <roylopezdev@gmail.com>
2024-03-07 15:55:31 -0300

    Add SocialLinks component to Footer

---
+++ src/components/Footer/SocialLinks.tsx
@@ -0,0 +1,21 @@
+import { socialLinks } from "@/src/constants/SOCIAL_LINKS";
+
+const SocialLinks = () => {
+  return (
+    <div className="mt-8 flex items-center">
+      {socialLinks.map(({ label, href, svg }, index) => (
+        <a
+          href={href}
+          key={`${label}-${index}`}
+          className={`mr-4 inline-block h-6 w-6 transition-transform duration-200 hover:scale-125 ${
+            label === "Github" ? "fill-light dark:fill-dark" : ""
+          }`}
+        >
+          {svg}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default SocialLinks;
