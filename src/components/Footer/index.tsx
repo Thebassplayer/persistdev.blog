b8d6cc247c7309cba8656f5b617e57c2ab4ecdb6
Roy Lopez <roylopezdev@gmail.com>
2024-03-07 15:58:49 -0300

    Refactor Footer component styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,13 +5,13 @@ import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
   return (
-    <footer className="m-5 mt-8 flex flex-col items-center rounded-2xl bg-dark text-light dark:bg-accentDark/90 dark:text-dark sm:m-10 sm:mt-16">
-      <h3 className="font-md mt-8 px-2 text-center text-2xl capitalize  dark:font-bold sm:mt-12 sm:text-3xl lg:text-4xl">
+    <footer className="m-5 mt-8 flex flex-col items-center rounded-md bg-dark text-light dark:bg-accentDark/90 dark:text-dark sm:m-10 sm:mt-16">
+      <h3 className="font-md mt-6 px-2 text-center text-2xl capitalize  dark:font-bold sm:mt-8 sm:text-3xl lg:text-4xl">
         Interesting Stories | Updates | Guides
       </h3>
       <SuscriptionForm />
       <SocialLinks />
-      <div className="relative mt-8 flex w-full flex-col items-center justify-between border-t border-solid border-light px-8 py-6 font-medium dark:border-dark md:flex-row">
+      <div className="relative mt-8 flex w-full flex-col items-center justify-between border-t border-solid border-light px-8 py-2 font-medium dark:border-dark md:flex-row">
         <span className="text-center text-xs md:text-base">
           &copy;2024 3R. All rights reserved.
         </span>
