bcfb1516c7d068d0d94fc7d707f2766e30ea524e
Roy Lopez <roylopezdev@gmail.com>
2024-04-02 17:09:12 -0300

    Remove Skills component from About page

--- src/app/(about)/about/page.tsx
+++ src/app/(about)/about/page.tsx
@@ -1,5 +1,4 @@
 import AboutCoverSection from "@/src/components/About/AboutCoverSection";
-import Skills from "@/src/components/About/Skills";
 import { siteMetadata } from "@/src/utils/siteMetadata";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -11,22 +10,7 @@ export const metadata: Metadata = {
 };
 
 const About = () => {
-  return (
-    <>
-      <AboutCoverSection />
-      <Skills />
-      <h2 className="mx-5 mt-8 self-start text-lg font-semibold text-dark dark:font-normal dark:text-light xs:mx-10 sm:mx-12 md:mx-16 md:text-2xl lg:mx-20">
-        Have a project in mind? Reach out to me 📞 from{" "}
-        <Link
-          href="/contact"
-          className="text-red-700 !underline underline-offset-2"
-        >
-          here
-        </Link>{" "}
-        and let's make it happen.
-      </h2>
-    </>
-  );
+  return <AboutCoverSection />;
 };
 
 export default About;
