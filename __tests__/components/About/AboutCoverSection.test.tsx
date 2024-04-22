029c1fbe3313157b636409138f2299af92be068d
Roy Lopez <ryhsu2@gmail.com>
2024-04-22 15:06:16 -0300

    Add unit tests for AboutCoverSection component

---
+++ __tests__/components/About/AboutCoverSection.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import AboutCoverSection from "@/src/components/About/AboutCoverSection";
+
+describe("AboutCoverSection", () => {
+  it("renders without crashing", () => {
+    render(<AboutCoverSection />);
+  });
+
+  it("displays the correct text content", () => {
+    render(<AboutCoverSection />);
+    expect(
+      screen.getByText(
+        "Never stop learning, because life never stops teaching.",
+      ),
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Hello! I´m Roy Lopez/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/Hope you enjoy my blog and find it useful/i),
+    ).toBeInTheDocument();
+  });
+
+  it("displays the image with correct alt text", () => {
+    render(<AboutCoverSection />);
+    expect(
+      screen.getByAltText("Roy codign in a laptop computer"),
+    ).toBeInTheDocument();
+  });
+});
