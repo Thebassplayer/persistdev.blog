b770d8cc9a48718176bca0d3d4a8421e4d67750f
Roy Lopez <ryhsu2@gmail.com>
2024-11-10 10:43:46 +0100

    Refactor HomeCoverSection to simplify image path handling and link generation for post tags

--- src/components/Home/HomeCoverSection.tsx
+++ src/components/Home/HomeCoverSection.tsx
@@ -14,6 +14,9 @@ const HomeCoverSection = ({ posts }: HomeCoverSectionProps) => {
   const sortedPosts = sortPosts(posts);
   const latestPost = sortedPosts[0];
   const firstPostTag = latestPost?.tags?.[0];
+  const linkTag = firstPostTag ? `/categories/${slug(firstPostTag)}` : "#";
+  const coverImage = latestPost?.image?.filePath.replace("../public", "") ?? "";
+  const bluredImage = latestPost?.image?.blurhashDataUrl;
 
   return (
     <div className="inline-block w-full">
@@ -21,10 +24,10 @@ const HomeCoverSection = ({ posts }: HomeCoverSectionProps) => {
         <div className="absolute bottom-0 left-0 right-0 top-0 z-0 h-full rounded-3xl bg-gradient-to-b from-transparent from-0% to-dark/90" />
         {latestPost?.image ? (
           <Image
-            src={latestPost.image?.filePath.replace("../public", "")}
+            src={coverImage}
             alt={latestPost.title}
             placeholder="blur"
-            blurDataURL={latestPost.image?.blurhashDataUrl}
+            blurDataURL={bluredImage}
             fill={true}
             className="-z-10 h-full w-full rounded-3xl object-cover object-center"
             priority
@@ -33,10 +36,7 @@ const HomeCoverSection = ({ posts }: HomeCoverSectionProps) => {
         ) : null}
         {firstPostTag ? (
           <div className="z-0 flex w-full flex-col items-start justify-center p-6 text-light sm:p-8 md:p-12 lg:w-3/4 lg:p-16">
-            <ButtonTag
-              link={`/categories/${slug(firstPostTag)}`}
-              name={firstPostTag}
-            />
+            <ButtonTag link={linkTag} name={firstPostTag} />
             <Link href={latestPost.url} className="mt-6">
               <h1 className="md-text-3xl text-lg font-bold capitalize sm:text-xl lg:text-4xl">
                 <span className="bg-gradient-to-r from-accent to-accent bg-[length:0px_5px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_5px] dark:from-accentDark dark:to-accentDark/50">
