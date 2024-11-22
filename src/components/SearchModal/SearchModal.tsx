03d30a3fb3cde8146a41be215b4f4662306da557
Roy Lopez <ryhsu2@gmail.com>
2024-11-22 07:21:25 +0100

    Add clear search functionality to SearchModal with button to reset search term

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -109,6 +109,11 @@ export function SearchModal({ posts }: SearchModalProps) {
     [handleSearch],
   );
 
+  const clearSearch = useCallback(() => {
+    setSearchTerm("");
+    handleSearch("");
+  }, [handleSearch, setSearchTerm]);
+
   return (
     <>
       {search && (
@@ -136,6 +141,13 @@ export function SearchModal({ posts }: SearchModalProps) {
                     className="text-gray-700 
                     w-full rounded-md px-4 py-2 pl-10 shadow-[rgba(0,_0,_0,_0)_0px_0px_8px] shadow-light focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-dark dark:text-light"
                   />
+                  <button
+                    title="Clear search"
+                    onClick={clearSearch}
+                    className="text-gray-500 hover:text-gray-700 absolute right-3 top-2 dark:text-light/50 dark:hover:text-light"
+                  >
+                    <XIcon className="h-6 w-6" />
+                  </button>
                   <SearchIcon className="absolute left-3 top-2.5 h-5 w-5 dark:text-light" />
                 </div>
                 <div className="max-h-96 overflow-y-auto">
