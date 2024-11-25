7670c57f73a7b292d42ec2c7bda7501c1e97f41f
Roy Lopez <ryhsu2@gmail.com>
2024-11-25 18:35:47 +0100

    Refactor SearchModal to use SearchInput component for improved input handling and clarity

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -8,6 +8,7 @@ import { Post } from "@/.contentlayer/generated";
 import { XIcon, SearchIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+import { SearchInput } from "../SearchInput/SearchInput";
 
 type SearchModalProps = {
   posts: Post[];
@@ -19,7 +20,7 @@ export function SearchModal({ posts }: SearchModalProps) {
   const searchParams = useSearchParams();
   const searchModal = searchParams.get("search-modal");
   const [searchResults, setSearchResults] = useState<FuseResult<Post>[]>([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const inputRef = useRef<HTMLInputElement>(null);
 
   const fuse = useMemo(
@@ -157,25 +158,11 @@ export function SearchModal({ posts }: SearchModalProps) {
             }}
           >
             <div className="p-6">
-              <div className="relative pb-4">
-                <input
-                  ref={inputRef}
-                  type="text"
-                  placeholder="Search posts..."
-                  value={searchTerm}
-                  onChange={handleInputChange}
-                  className="text-gray-700 
-                    w-full rounded-md px-4 py-2 pl-10 shadow-[rgba(0,_0,_0,_0)_0px_0px_8px] shadow-light focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-dark dark:text-light"
-                />
-                <button
-                  title="Clear search"
-                  onClick={clearSearch}
-                  className="text-gray-500 hover:text-gray-700 absolute right-3 top-2 dark:text-light/50 dark:hover:text-light"
-                >
-                  <XIcon className="h-6 w-6" />
-                </button>
-                <SearchIcon className="absolute left-3 top-2.5 h-5 w-5 dark:text-light" />
-              </div>
+              <SearchInput
+                value={searchTerm}
+                onChange={handleInputChange}
+                onClear={clearSearch}
+              />
               <div className="max-h-96 overflow-y-auto">
                 {searchResults.length > 0 ? (
                   <ul className="w-full space-y-6 first:pt-4">
