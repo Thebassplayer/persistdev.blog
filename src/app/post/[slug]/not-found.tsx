f22eb3c534f6ae375e23621ca0a7ac290ecc5758
Roy Lopez <ryhsu2@gmail.com>
2024-11-26 08:26:45 +0100

    Add SearchRaulIcon component and update NotFound page with icon

--- src/app/post/[slug]/not-found.tsx
+++ src/app/post/[slug]/not-found.tsx
@@ -1,6 +1,9 @@
+import SearchRaulIcon from "@/src/components/SearchRaulIcon/SearchRaulIcon";
+
 const NotFound = () => {
   return (
-    <div className="*:font-mono flex grow flex-col items-center justify-center">
+    <div className="*:font-mono flex grow flex-col items-center justify-center gap-2">
+      <SearchRaulIcon size={150} />
       <h1 className=" text-white">404 - Page not found</h1>
       <a href="/" className="text-blue-400">
         Return Home
--- src/components/SearchRaulIcon/SearchRaulIcon.tsx
+++ src/components/SearchRaulIcon/SearchRaulIcon.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import RaulImg from "@/public/SearchRaul.png";
 
-const SearchRaulIcon = () => {
+type SearchRaulIconProps = {
+  size?: number;
+};
+
+const SearchRaulIcon = ({ size = 100 }: SearchRaulIconProps) => {
   return (
-    <Image src={RaulImg} alt="Search Raul Icon" width={100} height={100} />
+    <Image src={RaulImg} alt="Search Raul Icon" width={size} height={size} />
   );
 };
 
